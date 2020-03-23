8e3079b8a4e46a96b2a78ec5cd3f0720c7fb5d95
Qifan <plocircle@live.com>
2020-03-23 21:47:51 +0800

    fix openmp

--- Spartan/submmit.sh
+++ Spartan/submmit.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+if [ -z $1 ]; then
+  echo "[*] Useage: submmit.sh slurm.slurm"
+  exit 0
+fi
+
 echo "[*] Please run this script under the directory where it is."
 
 slurm_file=$1
-job_id=$(echo "Submitted batch job 15497286" | awk '{print $4}')
+job_id=$(sbatch "$slurm_file" | awk '{print $4}')
 output_name="slurm-$job_id.out"
 
 echo "[*] Waitting output ..."
@@ -11,4 +17,4 @@ while [ ! -f "$output_name" ]; do
   sleep 1
 done
 echo "[*] $slurm_file done"
-mv $output_name "$slurm_file.out"
+mv "$output_name" "$slurm_file.out"
